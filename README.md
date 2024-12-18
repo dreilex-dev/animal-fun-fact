aaa4830fc8508c27343a906e2f8aefd3ad47c1c2
Andrei Boaca <dreilex@Andreis-MacBook-Pro.local>
2024-12-18 13:47:09 +0100

    Initial commit - Added Animal Fun Fact project

---
+++ README.md
@@ -0,0 +1,30 @@
+# React + Vite
+
+Animal Fun Fact
+
+- This is a simple React application that displays images of animals and when an user clicks on an image it will display a random fact about the animal.
+
+How to run the project:
+    Prerequisites:
+    -node.js
+    -npm
+
+Steps:
+1.Clone the repository:
+    -git clone <ssh link>
+    -in terminal run cd animal-fun-fact
+
+2.Install dependencies:
+    -npm install
+
+3.Run the dev server:ß
+    -npm run dev
+
+4.Open the project in your browser using the link shown in the terminal after running the command in step number 3
+
+To build the project for production: npm run build
+
+Technologies Used:
+-React
+-Vite 
+-CSS
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { animals } from './animals';
+import './App.css';
+
+const App = () => {
+  const [selectedFact, setSelectedFact] = useState('');
+  const showBackground = true;
+
+  const displayFact = (e) => {
+    const clickedAnimal = e.target.alt;
+    const optionIndex = Math.floor(Math.random() * animals[clickedAnimal].facts.length);
+    const animalGeneratedFact = animals[clickedAnimal].facts[optionIndex];
+    setSelectedFact(animalGeneratedFact);
+  };
+
+  const images = Object.keys(animals).map((animal) => (
+    <img
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animals[animal].image}
+      role="button"
+      onClick={displayFact}
+    />
+  ));
+
+  return (
+    <div>
+      {showBackground && <img className="background" alt="ocean" src="/images/ocean.jpg" />}
+      <div className="animals">{images}</div>
+      <p id="fact">{selectedFact}</p>
+      <h1>{selectedFact === '' ? 'Click an animal for a fun fact' : 'Fun Fact!'}</h1>
+    </div>
+  );
+};
+
+export default App;
